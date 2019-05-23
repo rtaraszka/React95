aa19756bb472216920d1937d1c43df185f8f032e
Artur Bien <artur.bien@gmail.com>
2019-05-23 20:11:05 +0200

    added buttons to snackbar story

--- src/components/Snackbar/Snackbar.stories.js
+++ src/components/Snackbar/Snackbar.stories.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 
-import Snackbar from "./Snackbar";
+import { Snackbar, Button, Toolbar } from "../";
 
 storiesOf("Snackbar", module)
   .addDecorator(story => (
@@ -15,5 +15,15 @@ storiesOf("Snackbar", module)
     </div>
   ))
   .add("default", () => (
-    <Snackbar>Your post has been submitted successfully!</Snackbar>
+    <Snackbar>
+      Your post has been submitted successfully!
+      <Toolbar style={{ marginTop: "0.5rem", marginBottom: "-0.5rem" }}>
+        <Button variant="flat" fullWidth>
+          OK
+        </Button>
+        <Button variant="flat" fullWidth disabled>
+          Cancel
+        </Button>
+      </Toolbar>
+    </Snackbar>
   ));
